fd3d30ab35c1d81ef16d2c06aa070d8b9bfa2801
Instituto First Choice <juanbautistaromiti@hotmail.com>
2024-07-30 17:06:14 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-// Selecciona los elementos utilizando sus clases
 document.addEventListener("DOMContentLoaded", function() {
     const button1 = document.querySelector(".v9_22");
     const button2 = document.querySelector(".v9_23");
     const button3 = document.querySelector(".v9_24");
     const button4 = document.querySelector(".v9_51");
 
-    // Asocia funciones a los eventos de clic para cada botón
     button1.addEventListener("click", function() {
-        alert("Botón 1 presionado");
+        window.location.href = "pagina1.html"; // Cambia esto a la URL deseada
     });
 
     button2.addEventListener("click", function() {
-        alert("Botón 2 presionado");
+        window.location.href = "pagina2.html"; // Cambia esto a la URL deseada
     });
 
     button3.addEventListener("click", function() {
-        alert("Botón 3 presionado");
+        window.location.href = "pagina3.html"; // Cambia esto a la URL deseada
     });
 
     button4.addEventListener("click", function() {
-        alert("Botón 4 presionado");
+        window.location.href = "pagina4.html"; // Cambia esto a la URL deseada
     });
 });
+
