21626703f0addf204685522d6a19133c9e8874b7
Instituto First Choice <juanbautistaromiti@hotmail.com>
2024-08-30 17:11:25 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,34 @@
 window.sr = ScrollReveal();
 
-
+// pagina inicial
     sr.reveal('.v9_16', {
         duration: 3000,
     });
+
     sr.reveal('.v55_3', {
         duration: 3000,
     });
+
     sr.reveal('.v9_22', {
         duration: 3000,
         origin: 'bottom',
-        distance: '300px'
+        distance: '200px'
     });
-    sr.reveal('.v18_2', {
-        duration: 3000,
-        origin: 'bottom',
-        distance: '300px'
-    });
+    
     sr.reveal('.v9_24', {
         duration: 3000,
         origin: 'bottom',
-        distance: '300px'
+        distance: '200px'
     });
+
+    sr.reveal('.v18_2', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '200px'
+    });
+
     sr.reveal('.v9_51', {
         duration: 3000,
         origin: 'left',
-        distance: '300px'
+        distance: '200px'
     });
