d0591ac71bcc4f14125454415b09f91cd57a4855
Instituto First Choice <juanbautistaromiti@hotmail.com>
2024-08-30 17:51:36 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,111 @@
+window.sr = ScrollReveal();
+
+// pagina inicial
+    sr.reveal('.v9_16', {
+        duration: 3000,
+    });
+
+    sr.reveal('.v55_3', {
+        duration: 3000,
+    });
+    sr.reveal('p', {
+        duration: 3000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+//pagina sobre nosotros
+sr.reveal('.v9_30', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+
+
+// pagina clases
+    sr.reveal('.v9_32', {
+        duration: 3000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+// pagina ingles
+sr.reveal('.v35_6', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+// pagina italiano
+sr.reveal('.v35_8', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+//pagina robotica
+sr.reveal('.v35_10', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+sr.reveal('.v85_1', {
+    duration: 4000,
+    origin: 'left',
+    distance: '200px'
+});
+
+// pagina contactos
+sr.reveal('.v34_3', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+sr.reveal('.v60_99', {
+    duration: 3000,
+});
+
+//pagina eventos
+sr.reveal('.v34_2', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+
+
+// pagina halloween
+sr.reveal('.v1_11', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.v1_12', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.v1_13', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.v1_14', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.v1_15', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+});
+
+ScrollReveal().reveal('.v36_3', {
+    rotate: {
+        x: 100,
+        z: 500
+    }
+});
